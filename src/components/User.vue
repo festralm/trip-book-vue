9acc2219e887fbc95ff430630c5c62cbfd519beb
Alia Minnegaraeva <alia01minn@gmail.com>
2021-05-08 01:47:33 +0300

    first comit

---
+++ src/components/User.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="user-container">
+  <div class="id">
+    {{id}}
+  </div>
+  <div class="email">
+    {{email}}
+  </div>
+  <div class="bann">
+    <button>Заблокировать</button>
+  </div>
+  <div class="delete">
+    <button>Удалить</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "User"
+}
+</script>
+
+<style scoped>
+.user-container {
+  width: 100px;
+  height: 100px;
+  padding: 25px;
+  margin: 30px;
+  background-color: bisque;
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="users-container">
+    <user v-for="(user, index) in users" :key="index"/>
+  </div>
+</template>
+
+<script>
+import User from "./User";
+export default {
+  name: "Users",
+  components: {User},
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          email: "alia@gmail.com",
+        },
+        {
+          id: 2,
+          email: "kate@gmail.com",
+        },
+        {
+          id: 3,
+          email: "max@gmail.com",
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
